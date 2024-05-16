15f376a6590cf45b4a58bf12c07ba8254b4929ef
Juan Cruz <juan_pineda_12@hotmail.es>
2024-05-16 20:20:11 -0300

    commit

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,30 @@
+
+
+export type BudgetActions = 
+    {type: "add-budget", payload: {budget : number}}
+
+export type BudgetState = {
+    budget: number
+}
+
+export const initialState : BudgetState = {
+    budget: 0
+}
+
+export const budgetReducer = (
+        state : BudgetState = initialState,
+        action : BudgetActions
+    ) => {
+
+    if(action.type === "add-budget") {
+        console.log("hola")
+
+        return {
+            ...state,
+            budget: action.payload.budget
+            
+        }
+    }
+
+    return state
+}
